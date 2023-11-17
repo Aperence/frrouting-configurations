f73eb780ea8eb85c79283835d6b4826b224cf23b
Aperence <anthony.doeraene@hotmail.com>
2023-11-17 18:45:49 +0100

    configured segment routing

--- setup.sh
+++ setup.sh
@@ -4,3 +4,15 @@ bgpd -d
 vtysh -f conf.txt
 sysctl net.ipv4.conf.all.forwarding=1
 sysctl net.ipv6.conf.all.forwarding=1
+sysctl net.mpls.platform_labels=100000
+sysctl net.mpls.conf.eth1.input=1
+sysctl net.mpls.conf.eth2.input=1
+if [ -n "$(ip addr | grep eth3)" ]; then
+    sysctl net.mpls.conf.eth3.input=1
+fi
+if [ -n "$(ip addr | grep eth4)" ]; then
+    sysctl net.mpls.conf.eth4.input=1
+fi
+if [ -n "$(ip addr | grep eth5)" ]; then
+    sysctl net.mpls.conf.eth5.input=1
+fi
