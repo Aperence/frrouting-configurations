d8e37d5a58aaf171621711e916899caa17b12a08
Aperence <anthony.doeraene@hotmail.com>
2023-11-19 18:18:03 +0100

    complexified a little bit topology

--- setup.sh
+++ setup.sh
@@ -16,3 +16,16 @@ fi
 if [ -n "$(ip addr | grep eth5)" ]; then
     sysctl net.mpls.conf.eth5.input=1
 fi
+if [ "$(hostname)" == "pe1" ] || [ "$(hostname)" == "pe3" ]; then
+    printf "\033[0;34m"
+    echo "Adding vrf Blue"
+    ip link add Blue type vrf table 10
+    ip link set dev Blue up
+fi
+if [ "$(hostname)" == "pe2" ] || [ "$(hostname)" == "pe4" ] then
+    printf "\033[0;31m"
+    echo "Adding vrf Red"
+    ip link add Red type vrf table 10
+    ip link set dev Red up
+fi
+printf '\033[0m'
