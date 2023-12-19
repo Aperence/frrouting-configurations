7b88114b408968a673183a85e59f8efca75f192d
Aperence <anthony.doeraene@hotmail.com>
2023-12-19 17:02:20 +0100

    added multicast (but not working)

---
+++ multicast/configs/setup-router1.sh
@@ -0,0 +1,9 @@
+zebra -d
+isisd -d
+pimd -d
+sysctl net.ipv4.ip_forward=1
+sysctl net.ipv4.conf.all.forwarding=1
+sysctl net.ipv6.conf.all.forwarding=1
+tc qdisc add dev eth1 parent root netem delay 5ms
+tc qdisc add dev eth2 parent root netem delay 5ms
+vtysh -f conf.txt
