693b3069a8aae700f94607447db84c138929c4a4
Aperence <anthony.doeraene@hotmail.com>
2023-12-21 11:00:20 +0100

    tries

--- vpn/setup.sh
+++ vpn/setup.sh
@@ -17,24 +17,43 @@ sysctl net.ipv6.conf.all.seg6_enabled=1
 sysctl net.ipv6.conf.eth1.seg6_enabled=1
 sysctl net.ipv6.conf.eth2.seg6_enabled=1
 
+# Adding the encapsulation
+if [ $(hostname) != "pe1" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.1/32 encap seg6 mode encap segs fc00:1:a:: dev sr0
+fi
+if [ $(hostname) != "pe2" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.2/32 encap seg6 mode encap segs fc00:2:a:: dev sr0
+fi
+if [ $(hostname) != "pe3" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.3/32 encap seg6 mode encap segs fc00:3:a:: dev sr0
+fi
+if [ $(hostname) != "pe4" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.4/32 encap seg6 mode encap segs fc00:4:a:: dev sr0
+fi
+if [ $(hostname) != "p1" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.10/32 encap seg6 mode encap segs fc00:10:a:: dev sr0
+fi
+if [ $(hostname) != "rr" ] && [ $(hostname | head -c 2) != "ce" ]; then
+    ip route add 10.255.0.20/32 encap seg6 mode encap segs fc00:20:a:: dev sr0
+fi
 
-if [ $(hostname) != "pe1" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.1/32 encap seg6 mode encap segs fc00:1:: dev lo
+if [ $(hostname) == "pe1" ]; then
+    ip -6 route add fc00:1:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
-if [ $(hostname) != "pe2" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.2/32 encap seg6 mode encap segs fc00:2:: dev lo
+if [ $(hostname) == "pe2" ]; then
+    ip -6 route add fc00:2:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
-if [ $(hostname) != "pe3" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.3/32 encap seg6 mode encap segs fc00:3:: dev lo
+if [ $(hostname) == "pe3" ]; then
+    ip -6 route add fc00:3:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
-if [ $(hostname) != "pe4" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.4/32 encap seg6 mode encap segs fc00:4:: dev lo
+if [ $(hostname) == "pe4" ]; then
+    ip -6 route add fc00:4:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
-if [ $(hostname) != "p1" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.10/32 encap seg6 mode encap segs fc00:10:: dev lo
+if [ $(hostname) == "p1" ]; then
+    ip -6 route add fc00:10:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
-if [ $(hostname) != "rr" ] && [$(hostname | head -c 2) != "ce"]; then
-    ip route add 10.255.0.20/32 encap seg6 mode encap segs fc00:20:: dev lo
+if [ $(hostname) == "rr" ]; then
+    ip -6 route add fc00:20:a:: encap seg6local action End.DX4 nh4 0.0.0.0 dev sr0
 fi
 
 if [ -n "$(ip addr | grep eth3)" ]; then
