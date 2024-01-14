0596bdcd67dfb973650246e5660033e61e1bb563
Aperence <anthony.doeraene@hotmail.com>
2024-01-14 10:56:54 +0100

    fixed a bug where != VPNs couldn't use the same prefix

--- vpn/03-configs-rr/pe1.sh
+++ vpn/03-configs-rr/pe1.sh
@@ -4,3 +4,8 @@ bgpd -d
 vtysh -f conf.txt
 sysctl net.ipv4.conf.all.forwarding=1
 sysctl net.ipv6.conf.all.forwarding=1
+
+for itf in $(ls /sys/class/net); do 
+    sysctl net.mpls.conf.$itf.input=1
+    sysctl net.ipv6.conf.$itf.seg6_enabled=1
+done
